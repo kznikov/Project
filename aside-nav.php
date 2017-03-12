1e78327fe432f2de2fb8c19c72aec07d22f4b8ac
DenitsaDremsizova <denitsa.dremsizova@gmail.com>
2017-03-12 20:53:45 +0200

    added aside menu

---
+++ aside-nav.php
@@ -0,0 +1,346 @@
+<aside class="aside-wrapper">
+    <h4 id="aside-title">Категории продукти</h4>
+    <button class="accordion">Лаптопи</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href="#">HP</a></li>
+            <li><a href="#">ASUS</a></li>
+            <li><a href="#">ACER</a></li>
+            <li><a href="#">Packard Bell</a></li>
+            <li><a href="#">до 14.9"</a></li>
+            <li><a href="#">от 15" до 16.9"</a></li>
+            <li><a href="#">над 17"</a></li>
+            <li><a href="#">Lenovo</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Таблети</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href="#">ACER</a></li>
+            <li><a href="#">Tablet</a></li>
+            <li><a href="#">HP</a></li>
+            <li><a href="#">ASUS</a></li>
+            <li><a href="#">Radius</a></li>
+            <li><a href="#">LG</a></li>
+            <li><a href="#">Lenovo</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Монитори</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href="#">ASUS</a></li>
+            <li><a href="#">BENQ</a></li>
+            <li><a href="#">ACER</a></li>
+            <li><a href="#">от 20" до 22"</a></li>
+            <li><a href="#">LG</a></li>
+            <li><a href="#">Packard Bell</a></li>
+            <li><a href="#">от 17" до 19"</a></li>
+            <li><a href="#">HP</a></li>
+            <li><a href="#">от 23" до 30"</a></li>
+            <li><a href="#">Аксесоари за монитори</a></li>
+            <li><a href="#">над 30"</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Дискове</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href="#">Kingston</a></li>
+            <li><a href="#">Външни дискове</a></li>
+            <li><a href="#">A-DATA</a></li>
+            <li><a href="#">HP</a></li>
+            <li><a href="#">SSD дискове</a></li>
+            <li><a href="#">За десктоп компютри</a></li>
+            <li><a href="#">Intel</a></li>
+            <li><a href="#">Western Digital</a></li>
+            <li><a href="#">Seagate</a></li>
+            <li><a href="#">За сървъри</a></li>
+            <li><a href="#">LG</a></li>
+            <li><a href="#">HITACHI</a></li>
+            <li><a href="#">Silicon Power</a></li>
+            <li><a href="#">За лаптопи</a></li>
+            <li><a href="#">Supermicro</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Flash памети</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href="#">A-DATA</a></li>
+            <li><a href="#">USB флаш памет</a></li>
+            <li><a href="#">Флаш-карта</a></li>
+            <li><a href="#">Kingston</a></li>
+            <li><a href="#">Silicon Power</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Телефони</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>NOKIA</a></li>
+            <li><a href='#'>Lenovo</a></li>
+            <li><a href='#'>Nokia Outlet</a></li>
+            <li><a href='#'>Microsoft</a></li>
+            <li><a href='#'>Motorola</a></li>
+            <li><a href='#'>LG</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Принтери</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>Лазерни</a></li>
+            <li><a href='#'>Canon</a></li>
+            <li><a href='#'>Мастиленоструйни</a></li>
+            <li><a href='#'>Многофункционални</a></li>
+            <li><a href='#'>Широкоформатни (Плотери)</a></li>
+            <li><a href='#'>Аксесоари</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Консумативи</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>За Лазерни принтери</a></li>
+            <li><a href='#'>За Мастиленоструйни принтери</a></li>
+            <li><a href='#'>Хартии и Филми</a></li>
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>Canon</a></li>
+            <li><a href='#'>За Плотери</a></li>
+            <li><a href='#'>Преоценени</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Аксесоари</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>Мишки</a></li>
+            <li><a href='#'>Клавиатури</a></li>
+            <li><a href='#'>Чанти и Калъфи</a></li>
+            <li><a href='#'>Батерии</a></li>
+            <li><a href='#'>Докинг станции</a></li>
+            <li><a href='#'>За лаптопи</a></li>
+            <li><a href='#'>Камери</a></li>
+            <li><a href='#'>Колонки</a></li>
+            <li><a href='#'>Слушалки</a></li>
+            <li><a href='#'>Микрофони</a></li>
+            <li><a href='#'>Джойстик</a></li>
+            <li><a href='#'>Предпазители</a></li>
+            <li><a href='#'>За телефони</a></li>
+            <li><a href='#'>За сървъри</a></li>
+            <li><a href='#'>Звукови карти</a></li>
+            <li><a href='#'>За Десктоп компютри</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Десктоп компютри</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>Sapphfire</a></li>
+            <li><a href='#'>ACER</a></li>
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>ASUS</a></li>
+            <li><a href='#'>Intel</a></li>
+            <li><a href='#'>Thin Clients</a></li>
+            <li><a href='#'>Lenovo</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Работни станции</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href="#">HP</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion no-items-nav">Скенери</button>
+
+
+    <button class="accordion">Мрежови продукти</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>Рутери</a></li>
+            <li><a href='#'>Суичове</a></li>
+            <li><a href='#'>Мрежови адаптери</a></li>
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>ASUS</a></li>
+            <li><a href='#'>D-Link</a></li>
+            <li><a href='#'>Supermicro</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Памети</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>За десктоп компютри</a></li>
+            <li><a href='#'>За лаптопи</a></li>
+            <li><a href='#'>За сървъри</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Фотоапарати</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>KODAK</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Видео карти</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>ASUS</a></li>
+            <li><a href='#'>Gigabyte</a></li>
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>Palit</a></li>
+            <li><a href='#'>Sapphire</a></li>
+            <li><a href='#'>KFA2</a></li>
+            <li><a href='#'>PowerColor</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Процесори</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>Intel</a></li>
+            <li><a href='#'>AMD</a></li>
+            <li><a href='#'>Аксесоари за процесори</a></li>
+            <li><a href='#'>HP</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Проектори</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>BENQ</a></li>
+            <li><a href='#'>CANON</a></li>
+            <li><a href='#'>ACER</a></li>
+            <li><a href='#'>LG</a></li>
+            <li><a href='#'>ASUS</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion no-items-nav">UPS устройства</button>
+    
+
+    <button class="accordion">Дънни платки</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>ASROCK</a></li>
+            <li><a href='#'>SAPPHIRE</a></li>
+            <li><a href='#'>ASUS</a></li>
+            <li><a href='#'>Gigabyte</a></li>
+            <li><a href='#'>Intel</a></li>
+            <li><a href='#'>Supermicro</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Сървъри</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>Supermicro</a></li>
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>Сървърни опции</a></li>
+            <li><a href='#'>Софтуер</a></li>
+            <li><a href='#'>Аксесоари</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Вентилатори</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>Cooler Master</a></li>
+            <li><a href='#'>GEIL</a></li>
+            <li><a href='#'>EVERCOOL</a></li>
+            <li><a href='#'>ARCTIC</a></li>
+            <li><a href='#'>DAOTECH</a></li>
+            <li><a href='#'>Supermicro</a></li>
+            <li><a href='#'>Fractal Design</a></li>
+            <li><a href='#'>Fortron</a></li>
+            <li><a href='#'>Intel</a></li>
+            <li><a href='#'>NZXT</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Кутии</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>KME</a></li>
+            <li><a href='#'>Cooler Master</a></li>
+            <li><a href='#'>DAOTECH</a></li>
+            <li><a href='#'>Fractal Design</a></li>
+            <li><a href='#'>Supermicro</a></li>
+            <li><a href='#'>Fortron</a></li>
+            <li><a href='#'>Аксесоари за кутии</a></li>
+            <li><a href='#'>NZXT</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Захранващи модули</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>Fortron</a></li>
+            <li><a href='#'>Cooler Master</a></li>
+            <li><a href='#'>Seasonic</a></li>
+            <li><a href='#'>NZXT</a></li>
+            <li><a href='#'>Supermicro</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Контролери и кабели</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>Supermicro</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Софтуер</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>Supermicro</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion">Сториджи</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>HP</a></li>
+            <li><a href='#'>Supermicro</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion no-items-nav">Оптични устройства</button>
+
+    <button class="accordion">Допълнителна гаранция</button>
+    <div class="panel">
+        <ul class="aside-sub-nav">
+            <li><a href='#'>За лаптопи</a></li>
+            <li><a href='#'>За сървъри</a></li>
+            <li><a href='#'>За десктоп компютри</a></li>
+        </ul>
+    </div>
+
+    <button class="accordion no-items-nav">Смарт часовници</button>
+
+</aside>
+
+<script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        }
+    }
+</script>
